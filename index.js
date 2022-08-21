3e8112b79497fb02b065cd6ebe67dec49c055d0b
Doreen <doreenlulu97@gmail.com>
2022-08-21 23:39:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(defaultActivity = 'roller-skate') {
+    return `This Saturday, I want to ${defaultActivity}!`;
+}
+
+function mondayWork(defaultActivity = 'go to the office') {
+    return `This Monday, I will ${defaultActivity}.`;
+}  
+
+function wrapAdjective(emphatic){
+    return function (worker) {
+        return `You are ${emphatic}${worker}${emphatic}!`;
+    }
+}
+console.log(wrapAdjective('||')); 
